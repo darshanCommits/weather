02569391124dd47d07b210e4f7530d064170398d
Darshan Kumawat <kumawatdarshan.1304@gmail.com>
2023-08-30 14:00:02 +0530

    handler for multiple cities

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const city = "Mawsynram";
+// const city = "Mawsynram";
+const id = document.querySelector("#weatherData");
+const inputQuery = document.querySelector("form input[type=text]");
+const form = document.querySelector("form");
+const dataList = document.querySelector("#datalist")
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  id.classList.add("not-loaded")
+  const city = inputQuery.value.trim();
+  // console.log(city)
+  await updateWeatherData(city);
+  id.classList.add("loaded");
+})
 
 async function getCoordinates(city) {
   try {
@@ -10,6 +23,9 @@ async function getCoordinates(city) {
       throw new Error("Network response : not okay.");
     if (searchResults.length === 0)
       throw new Error("No such city found!!")
+    // if (searchResults.length !== 1) {
+    //   createDropdownMenu(cityList)
+    // }
 
     const lat = searchResults[0].lat;
     const lon = searchResults[0].lon;
@@ -22,12 +38,22 @@ async function getCoordinates(city) {
   }
 }
 
-async function getWeatherData({ lat, lon }, { hourlyParam, dailyParam }) {
+function createDropdownMenu(cityList, parent) {
+  cityList.forEach(city => {
+    const option = document.createElement("option");
+    option.value = city;
+    parent.appendChild(option);
+  })
+}
+
+async function getWeatherData({ lat, lon }) {
   try {
+    const hourlyParam = `temperature_2m,relativehumidity_2m,apparent_temperature`;
+    const dailyParam = `weathercode,temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum`;
     const apiEndpoint = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=${hourlyParam}&daily=${dailyParam}&timezone=auto`
     const fetchPromise = await fetch(apiEndpoint, { mode: "cors" });
     const jsonResponse = await fetchPromise.json();
-    console.log(jsonResponse)
+    //     console.log(jsonResponse)
     if (!fetchPromise.ok) {
       throw new Error("Network response : Not Okay.\nTry checking typos in apiEndpoint variable.");
     }
@@ -38,7 +64,7 @@ async function getWeatherData({ lat, lon }, { hourlyParam, dailyParam }) {
       rainUnit: jsonResponse.daily_units.rain_sum,
       snowUnit: jsonResponse.daily_units.snowfall_sum,
     };
-    console.log(unit)
+    //     console.log(unit)
 
     const apparentTemp = jsonResponse.hourly.apparent_temperature[0] + unit.tempUnit;
     const currentTemp = jsonResponse.hourly.temperature_2m[0] + unit.tempUnit;
@@ -59,43 +85,44 @@ async function getWeatherData({ lat, lon }, { hourlyParam, dailyParam }) {
   }
 }
 
-async function main() {
-  const hourlyParam = `temperature_2m,relativehumidity_2m,apparent_temperature`;
-  const dailyParam = `weathercode,temperature_2m_max,temperature_2m_min,rain_sum,snowfall_sum`;
-  const parameters = { hourlyParam, dailyParam };
-
+async function updateWeatherData(city) {
   const coordinates = await getCoordinates(city);
-  const weatherData = await getWeatherData(coordinates, parameters);
-  console.log(weatherData);
-  getHtml(weatherData)
+  const weatherData = await getWeatherData(coordinates);
+  const htmlMarkup = getHtml(weatherData, city);
+
+  id.innerHTML = htmlMarkup;
 }
 
-async function getHtml(weatherData) {
+function getHtml(weatherData, city) {
   const htmlMarkup = `
-      <div>
-        <h1>Temprature </h1>
-        <h1>${weatherData.currentTemp}</h1>
-      </div>
-      <div>
-        <h1>Feels like </h1>
-        <h1>${weatherData.apparentTemp}</h1>
-      </div>
-      <div>
-        <h1>Humidity </h1>
-        <h1>${weatherData.humidity}</h1>
-      </div>
-      <div>
-        <h1>Rain </h1>
-        <h1>${weatherData.rain}</h1>
-      </div>
-      <div>
-        <h1>Snow </h1>
-        <h1>${weatherData.snow}</h1>
-      </div>
-  `
-  console.log(htmlMarkup)
+    <h1>${city}</h1>
+    <div>
+      <h1>Temprature </h1>
+      <h1>${weatherData.currentTemp}</h1>
+    </div>
+    <div>
+      <h1>Feels like </h1>
+      <h1>${weatherData.apparentTemp}</h1>
+    </div>
+    <div>
+      <h1>Humidity </h1>
+      <h1>${weatherData.humidity}</h1>
+    </div>
+`
+  if (weatherData.rain !== "0mm")
+    htmlMarkup += `
+    <div>
+      <h1>Rain </h1>
+      <h1>${weatherData.rain}</h1>
+    </div>
+`
+  if (weatherData.snow !== "0cm")
+    htmlMarkup += `
+    <div>
+      <h1>Snow </h1>
+      <h1>${weatherData.snow}</h1>
+    </div>
+`
+  return htmlMarkup;
 }
 
-main();
-const id = document.querySelector(".weather");
-console.log(id);
